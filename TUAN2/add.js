2ab22e76b2429ef675ef85578e4c5034984fe080
nhan4122006 <nhan4122006@gmail.com>
2023-11-05 09:39:31 +0700

    jsa

---
+++ TUAN2/add.js
@@ -0,0 +1,33 @@
+//  3 kieu  khai bao 1 bien
+//var
+//let
+//const
+
+// 6 kieu du lieu trong js
+//number
+//string
+//boolean
+//array
+//object
+//null
+//undefind
+
+// in kieu du lieu ra bang
+//typeof
+
+// 3 vong lap co ban
+//for
+//while
+//do while
+
+
+
+
+
+
+
+
+
+
+
+
